4e54dbf2a578be5be981557429d17a89c4bb2396
PavRazgulin <121128137+PavRazgulin@users.noreply.github.com>
2022-12-20 22:54:12 +0300

    Add files via upload

---
+++ Sem1.cs
@@ -0,0 +1,43 @@
+// Написать программу, которая на входе получает три числа, а на выходе определяет, какое из них больше. 
+//{
+//Console.WriteLine("Введите первое число");
+//int a = ConvertToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int в = ConvertToInt32(Console.ReadLine());
+//Console.WriteLine("Введите третье число");
+//int с = ConvertToInt32(Console.ReadLine());
+//max=a;
+      
+    //if ( a>b); 
+      //  {
+        //if (a>c)
+        //max= a;
+        //}
+    //if (b>a);
+      //  {
+       //if (b>c);
+//       max= b;
+//       }
+//    if (c>a);
+//       {
+ //      if (c>b);
+//       max=c;
+//       }
+//
+//Console.WriteLine($"Это число максимальное {max}");
+//}
+
+// Написать программу, которая определяет, четное ли введённое число
+
+//Console.WriteLine("Введите число");
+//int a = ConvertToInt32(Console.ReadLine(num));
+
+//if (a%2=0) 
+//{
+// Console.WriteLine($"Это число чётное");   
+//}
+//else 
+//{
+// Console.WriteLine($"Это число нечётное");
+//}
+
