3cb9b39163a967e6b065ddf743780320529eaaff
PavRazgulin <121128137+PavRazgulin@users.noreply.github.com>
2022-12-22 20:37:35 +0300

    Add files via upload

---
+++ Sem4.cs
@@ -0,0 +1,84 @@
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3?)
+
+// 2, 4 -> 16
+
+//int Stepen(int numA, int numB)
+//{
+//  int result = 1;
+//  for(int i=1; i <= numberB; i++){
+//        int result = Math.Pow(numA, numB)
+//        return result;
+//        }
+//{  
+//Console.WriteLine("Введите число A: ");
+//Int num A = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число B: ");
+//Int num B = Convert.ToInt32(Console.ReadLine());
+
+//Stepen (numA, numB);
+//int answer = Console.WriteLine($"Результат: {result} ");
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+//Console.Write("Введите число X: ");
+//int numberX = Convert.ToInt32(Console.ReadLine());
+
+  //int Summa(int numberX){
+    
+    //int counter = Convert.ToString(numberN).Length;
+    //int cutoff = 0;
+    //int result = 0;
+
+    //for (int i = 0; i < counter; i++){
+      //cutoff = numberX - numberX % 10;
+      //result = result + (numberX - cutoff);
+      //numberX = numberX / 10;
+    }
+   //return result;
+  }
+
+//int answer = Summa(numberX);
+//Console.WriteLine($"Сумма цифр в числе: {result}");
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+//массив нужного размера и заполненный случайными числами, которые нам нужны
+
+//int[] CreateRandomArray (int size, int minValue, int maxValue)
+//{
+//    int []myArray = new int[size];
+ //   for (int i=0; i<size;i++);
+//    {
+//        myArray[i] = new  int [];
+//    }
+//    return myArray;
+//}
+//void ShowArray (int[]array)
+//{
+//    Console.WriteLine("Полученный массив->");
+//    for (int i=0; i<array.Lenght;i++);
+//    {
+//        Console.Write(array[i]+ " ");
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("Введите числа массива");
+//int lenght = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите минимально возможное значение массива");
+//int lenght = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите максимально возможное значение массива");
+//int lenght = Convert.ToInt32(Console.ReadLine());
+
+//int [] newArray = CreateRandomArray(lenght,min,max);
+//ShowArray (newArray);
+
