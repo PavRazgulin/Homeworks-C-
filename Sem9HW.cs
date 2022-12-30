22325137282835a8c227647d46d54992ba300f21
PavRazgulin <121128137+PavRazgulin@users.noreply.github.com>
2022-12-30 06:19:26 +0300

    Add files via upload

---
+++ Sem9HW.cs
@@ -0,0 +1,61 @@
+// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// */
+
+// void PrintNum (int num) 
+// // {
+// //       if(num<=1)
+// //       PrintNum(num-1);
+// //       Console.WriteLine(Num - " ");
+// // }
+// Console.WriteLine. ("Введите число")
+// int n = ConvertToInt32(Console.ReadLine);
+// // PrintNum(num);
+
+
+
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Console.WriteLine("Введите натуральное число больше 1:");
+// int == int.Parse(Console.ReadLine());
+
+// int NumberShow (int num)
+// {
+//     if (number < 0) Console.Write($"{number} не натуральное число");
+//     if (number == 0) return;
+//     Console.Write("{0,4}", number);
+//     NumberCounter (number - 1);
+// }
+
+// NumberShow(number);
+
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         1 + + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return  (m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Akkerman(m, n);
+// Console.Write (Akkerman (m, n)); 
+
